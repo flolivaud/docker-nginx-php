bbdb577b4710d392d8db1c24f247dad84a38b8d4
Florent OLIVAUD <florent@coworkees.com>
2018-05-25 14:22:40 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <style type="text/css">
+            body {
+                display: flex;
+                justify-content: center; /* align horizontal */
+                align-items: center; /* align vertical */
+            }
+
+            .logo > img {
+                max-width: 500px;
+                height: auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="logo">
+            <img src="<?php echo 'coworkees_logo.png'; ?>" alt="<?php echo 'Coworkees Logo'; ?>">
+        </div>
+    </body>
+</html>
